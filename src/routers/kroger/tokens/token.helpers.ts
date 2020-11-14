9744d331ece6a3fca5354b0a249ac90051042f35
Dre mf Randaci <drerandaci@gmail.com>
2020-11-14 16:45:05 -0600

    [Chore] setup prettier + eslint, lint code

--- src/routers/kroger/tokens/token.helpers.ts
+++ src/routers/kroger/tokens/token.helpers.ts
@@ -1,6 +1,6 @@
 const clientId = process.env.CLIENT_ID;
 const clientSecret = process.env.CLIENT_SECRET;
-const scopes = process.env.KROGER_SCOPES
+const scopes = process.env.KROGER_SCOPES;
 import axios, { AxiosResponse } from 'axios';
 import qs from 'qs';
 
@@ -11,18 +11,21 @@ interface KrogerToken {
 }
 
 export const getToken = async (): Promise<AxiosResponse<KrogerToken>> => {
-  const creds = Buffer.from(`${clientId}:${clientSecret}`, `ascii`).toString('base64');
+  const creds = Buffer.from(`${clientId}:${clientSecret}`, `ascii`).toString(
+    'base64',
+  );
 
   const headers = {
-    headers:{
-    'Content-Type': 'application/x-www-form-urlencoded',
-    'Authorization': `Basic ${creds}`}
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+      Authorization: `Basic ${creds}`,
+    },
   };
 
   const body = {
     grant_type: 'client_credentials',
-    scope: scopes
-  }
+    scope: scopes,
+  };
 
   const tokenUrl = `${process.env.OAUTH2_BASE_URL}/token`;
 
@@ -30,6 +33,6 @@ export const getToken = async (): Promise<AxiosResponse<KrogerToken>> => {
     const response = await axios.post(tokenUrl, qs.stringify(body), headers);
     return response.data;
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-}
+};
--- src/routers/kroger/tokens/token.router.ts
+++ src/routers/kroger/tokens/token.router.ts
@@ -1,18 +1,15 @@
 import * as express from 'express';
-import { getToken } from "./token.helpers";
+import { getToken } from './token.helpers';
 const router = express.Router();
 
-router.get(
-  '/',
-  async (req: Request, res: express.Response) => {
-    const tokenResponse = await getToken();
-    const result = {
-        body: {
-            ...tokenResponse
-        }
-    }
-    res.status(res.statusCode).json(result.body);
-  },
-);
+router.get('/', async (req: Request, res: express.Response) => {
+  const tokenResponse = await getToken();
+  const result = {
+    body: {
+      ...tokenResponse,
+    },
+  };
+  res.status(res.statusCode).json(result.body);
+});
 
 export default router;
