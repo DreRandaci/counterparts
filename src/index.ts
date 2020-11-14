9e901bb68b1e70548608687560b2704871a60d67
Dre mf Randaci <drerandaci@gmail.com>
2020-11-14 16:27:47 -0600

    [Feature] setup tsconfig, nodemon, env vars

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import path from "path";
+require('dotenv').config({path: path.join(__dirname, '../.env')});
+const app = require('./app').default;
+
+try {
+    const port = process.env.HTTP_PORT || 3000;
+    app.listen(port, () => {
+        console.info(`api listening on port ${port}!`);
+    });
+} catch (err) {
+    console.error(`API COULD NOT START: ${JSON.stringify(err)}`);
+}
